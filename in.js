048f1be3633a0b03ee7fc3520f844f2c3baeef80
murenochka <murrizaur777@gmail.com>
2025-09-16 22:29:11 +0300

    Update in.js

--- in.js
+++ in.js
@@ -1,6 +1,7 @@
 /**
  * @type {Record<string | RegExp, string>}
  */
+alert('here2')
 let replacements = {};
 let dumpedVarNames = {};
 const storeName = "a" + crypto.randomUUID().replaceAll("-", "").substring(16);
